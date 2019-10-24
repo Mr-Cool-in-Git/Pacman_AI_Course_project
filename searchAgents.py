fa5e40f6040a71d7d6671f265d69396a02ed187d
Mr.Cool <kylin98@list.ru>
2019-10-24 17:23:46 +0200

    no problem

--- searchAgents.py
+++ searchAgents.py
@@ -286,23 +286,21 @@ class CornersProblem(search.SearchProblem):
                 print 'Warning: no food in corner ' + str(corner)
         self._expanded = 0 # DO NOT CHANGE; Number of search nodes expanded
         # Please add any code here which you would like to use
-        # in initializing the problem
-        "*** YOUR CODE HERE ***"
+        # in initializing the problemz
+        self.goal = self.corners
 
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return state == 2,2
 
     def getSuccessors(self, state):
         """
@@ -310,24 +308,12 @@ class CornersProblem(search.SearchProblem):
 
          As noted in search.py:
             For a given state, this should return a list of triples, (successor,
-            action, stepCost), where 'successor' is a successor to the current
-            state, 'action' is the action required to get there, and 'stepCost'
-            is the incremental cost of expanding to that successor
+            action, stepCost), where
+            'successor' is a successor to the current state,
+            'action' is the action required to get there, and
+            'stepCost' is the incremental cost of expanding to that successor
         """
-
-        successors = []
-        for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
-            # Add a successor state to the successor list if the action is legal
-            # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
-
-            "*** YOUR CODE HERE ***"
-
-        self._expanded += 1 # DO NOT CHANGE
-        return successors
+        return 0
 
     def getCostOfActions(self, actions):
         """
@@ -483,8 +469,6 @@ class ClosestDotSearchAgent(SearchAgent):
         food = gameState.getFood()
         walls = gameState.getWalls()
         problem = AnyFoodSearchProblem(gameState)
-
-
 
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
